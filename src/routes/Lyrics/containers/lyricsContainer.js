78b8bd52afe15cd541be1ac5e0de8200788f34ea
Wung <wungmathing@gmail.com>
2016-10-25 21:40:49 +0530

    add placeholder lyrics

--- src/routes/Lyrics/containers/lyricsContainer.js
+++ src/routes/Lyrics/containers/lyricsContainer.js
@@ -17,14 +17,110 @@ class LyricsContainer extends Component {
     });
 
     return (
-      <View>
-        <Text>
-          Count of Dogs in Realm: {realm.objects('Dog').length}
-        </Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.para}>
+            <Text style={styles.text}>
+              1 Ringkapha tuina, Jesu Iwui
+            </Text>
+            <Text style={styles.text}>
+              Varewui leikashi sachitheihao
+            </Text>
+            <Text style={styles.text}>
+              Morei pheomida ili huimi
+            </Text>
+            <Text style={styles.text}>
+              Chonmeili mida ili thanmi
+            </Text>
+          </View>
+          <View style={styles.para}>
+            <Text style={styles.text}>
+              2 Ringkapha tuina, Jesu Iwui
+            </Text>
+            <Text style={styles.text}>
+              Varewui leikashi sachitheihao
+            </Text>
+            <Text style={styles.text}>
+              Morei pheomida ili huimi
+            </Text>
+            <Text style={styles.text}>
+              Chonmeili mida ili thanmi
+            </Text>
+          </View>
+          <View style={styles.para}>
+            <Text style={styles.text}>
+              3 Ringkapha tuina, Jesu Iwui
+            </Text>
+            <Text style={styles.text}>
+              Varewui leikashi sachitheihao
+            </Text>
+            <Text style={styles.text}>
+              Morei pheomida ili huimi
+            </Text>
+            <Text style={styles.text}>
+              Chonmeili mida ili thanmi
+            </Text>
+          </View>
+          <View style={styles.para}>
+            <Text style={styles.text}>
+              4 Ringkapha tuina, Jesu Iwui
+            </Text>
+            <Text style={styles.text}>
+              Varewui leikashi sachitheihao
+            </Text>
+            <Text style={styles.text}>
+              Morei pheomida ili huimi
+            </Text>
+            <Text style={styles.text}>
+              Chonmeili mida ili thanmi
+            </Text>
+          </View>
+          <View style={styles.para}>
+            <Text style={styles.text}>
+              5 Ringkapha tuina, Jesu Iwui
+            </Text>
+            <Text style={styles.text}>
+              Varewui leikashi sachitheihao
+            </Text>
+            <Text style={styles.text}>
+              Morei pheomida ili huimi
+            </Text>
+            <Text style={styles.text}>
+              Chonmeili mida ili thanmi
+            </Text>
+          </View>
+          <View style={styles.para}>
+            <Text style={styles.text}>
+              6 Ringkapha tuina, Jesu Iwui
+            </Text>
+            <Text style={styles.text}>
+              Varewui leikashi sachitheihao
+            </Text>
+            <Text style={styles.text}>
+              Morei pheomida ili huimi
+            </Text>
+            <Text style={styles.text}>
+              Chonmeili mida ili thanmi
+            </Text>
+          </View>
+        </View>
+      </ScrollView >
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 15
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'left'
+  },
+  para: {
+    paddingTop: 20
+  }
+});
 
 
 const mapActionCreators = {
